e726e072ab8921ec53327cf7bc7ae7fff488b248
lilburne1 <benlilburne@outlook.com>
2024-05-20 17:24:11 +0800

    navigation guess

--- project2/src/otherNodes/launch/navigation.launch.py
+++ project2/src/otherNodes/launch/navigation.launch.py
@@ -12,19 +12,19 @@ def generate_launch_description():
     navigation_config_file = get_package_share_directory('otherNodes') + '/config/navigation.yaml'
     nav2_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'navigation_launch.py')
+            os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
         ),
         launch_arguments={
             'params_file': navigation_config_file
         }.items(),
     )
 
-    robot_position = Node(
-        package="otherNodes",
-        executable='robot_position'
-    )
+    # robot_position = Node(
+    #     package="otherNodes",
+    #     executable='robot_position'
+    # )
     
     return LaunchDescription([
-        nav2_launch,
-        robot_position
+        nav2_launch
+        # robot_position
     ])
